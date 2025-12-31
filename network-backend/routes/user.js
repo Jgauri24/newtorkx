4bf0eca7d991ff3492b291e255850e01ee718e3c
Jgauri24 <jindalgauri244@gmail.com>
2025-12-31 20:49:50 +0530

    apis rqeuired for user are created

--- network-backend/routes/user.js
+++ network-backend/routes/user.js
@@ -5,8 +5,10 @@ const Authentication = require("../authentication/auth")
 router.post("/register",userController.register)
 router.post("/login",userController.login)
 router.post("/google",userController.loginThroughGmail)
-
-router.get("/self",Authentication.auth,(req,res)=>{
-    console.log(req.user)
-})
+router.put("/update",Authentication.auth,userController.updateUser)
+router.get("/user/:id",userController.getProfileById)
+router.post("/logout",Authentication.auth,userController.logout)
+// router.get("/self",Authentication.auth,(req,res)=>{
+//     console.log(req.user)
+// })
 module.exports=router;
